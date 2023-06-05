dc3429fbe54a135632a6b1783609c30a4986fb6c
Akash Singh <85953541+AkashSingh37@users.noreply.github.com>
2023-06-05 23:48:05 +0530

    Update 0215-LargestElement.java

--- 0215-LargestElement.java
+++ 0215-LargestElement.java
@@ -1,6 +1,18 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        //PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        // for(int i=0; i<nums.length; i++){
+        //     pq.add(nums[i]);
+        // }
+        // int i=1;
+        // while(i<k){
+        //     pq.remove();
+        //     i++;
+        // }
+        // return pq.peek();
+        
         for(int i=0; i<nums.length; i++){
             pq.add(nums[i]);
             if(pq.size() > k){
