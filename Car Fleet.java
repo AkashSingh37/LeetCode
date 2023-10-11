72420d27f49684535cc9b4d9337bf9184b1b925e
Akash Singh <85953541+AkashSingh37@users.noreply.github.com>
2023-10-11 23:52:27 +0530

    Update Car Fleet.java

--- Car Fleet.java
+++ Car Fleet.java
@@ -1,1 +1,36 @@
+class Solution {
+    public static class Car {
+        public int position;
+        public int speed;
 
+        public Car(int position, int speed) {
+            this.position = position;
+            this.speed = speed;
+        }
+    }
+    
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        Car[] cars = new Car[n];
+
+        for (int i = 0; i < n; i++) {
+            cars[i] = new Car(position[i], speed[i]);
+        }
+
+        Arrays.sort(cars, (x, y) -> Integer.compare(y.position, x.position));
+
+        int fleetsCount = 0;
+        double lastCarArrivalTime = 0;
+
+        for (Car car : cars) {
+            double arrivalTime = (target - car.position) / (double) car.speed;
+
+            if (arrivalTime > lastCarArrivalTime) {
+                fleetsCount++; // new fleet formed
+                lastCarArrivalTime = arrivalTime;
+            }
+        }
+
+        return fleetsCount;
+    }
+}
