dd0dee184dccc40b8e99c8e9d05178c0932fba02
Akash Singh <85953541+AkashSingh37@users.noreply.github.com>
2023-10-11 23:56:05 +0530

    Update Max Chunks To Make Sorted.java

--- Max Chunks To Make Sorted.java
+++ Max Chunks To Make Sorted.java
@@ -1,1 +1,32 @@
+// class Solution {
+//     public int maxChunksToSorted(int[] arr) {
+//         int chunks = 1;
+//         if(arr.length == 1){
+//             return 1;
+//         }
+//         int max = arr[0];
+//         for(int i=0; i<arr.length; i++){
+//             if(max < arr[i]){
+//                 chunks++;
+//             }
+//             max = Math.max(arr[i], max);
+//         }
+//         return chunks;
+//     }
+// }
 
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int chunks = 0;
+        int max = 0;
+        
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+            if (max == i) {
+                chunks++;
+            }
+        }
+        
+        return chunks;
+    }
+}
