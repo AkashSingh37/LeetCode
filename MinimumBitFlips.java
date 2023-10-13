208403be5398ca9cde57ef812482359f64ba4cf9
Akash Singh <85953541+AkashSingh37@users.noreply.github.com>
2023-10-13 21:24:33 +0530

    Update MinimumBitFlips.java

--- MinimumBitFlips.java
+++ MinimumBitFlips.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        if(start == goal){
+            return 0;
+        }
+        int xor = start ^ goal;
+        int counter=0;
+        while(xor > 0) {
+            xor = xor & (xor-1);
+            counter++;
+        }
+        return counter;
+    }
+}
